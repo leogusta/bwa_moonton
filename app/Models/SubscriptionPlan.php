1747a43ee945e414d4e94bce94517f895fbe59a9
leogusta <leogusta91@gmail.com>
2024-06-24 11:35:38 +0700

    feat: migrations and seeder files

---
+++ app/Models/SubscriptionPlan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SubscriptionPlan extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'price', 'active_period_in_months', 'features'];
+
+    function subscriptionPlan(): BelongsTo
+    {
+        return $this->belongsTo(SubscriptionPlan::class);
+    }
+}
